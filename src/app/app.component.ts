8cef908f2a187df962cc94935b0cae2d1480fe9a
Syafiq <isyafiq@gmail.com>
2024-03-23 02:46:33 +0800

    add swipe js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { register } from 'swiper/element/bundle'
+import { SwiperContainer } from 'swiper/element/bundle';
+import { SwiperSlide } from 'swiper/element/bundle';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pokedex';
+  ngOnInit() {
+    register();
+  }
 }
