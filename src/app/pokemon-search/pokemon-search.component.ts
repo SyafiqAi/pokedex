dbf87f2604f7af0851232d7b25306f6fb1825f55
Syafiq <isyafiq@gmail.com>
2024-03-30 23:15:37 +0800

    working search

--- src/app/pokemon-search/pokemon-search.component.ts
+++ src/app/pokemon-search/pokemon-search.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { PokedexService } from '../pokedex.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-search',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule],
   templateUrl: './pokemon-search.component.html',
   styleUrl: './pokemon-search.component.css'
 })
 export class PokemonSearchComponent {
+  constructor(private pokedexService: PokedexService) { }
+
+  async ngOnInit() {
+    this.pokemonList = await this.pokedexService.getAllPokemonNames();
+  }
+
+  pokemonList: string[] = []
+  searchValue = ''
+  showModal = true;
+
+
+  handleInput() {
+    console.log(this.searchValue);
+  }
+
+  public get filteredList() {
+    const pokemonPerSearch = 10
+    const filteredList = this.pokemonList;
+    return filteredList.filter(p => p.includes(this.searchValue.toLowerCase())).splice(0,pokemonPerSearch);
+  }
+
+  toggleModal(show: boolean) {
+    this.showModal = show;
+
+    // For some reason focusing immediately doesn't work.
+    // Have to set a tiny delay.
+    setTimeout(() => {
+      const el = document.getElementById('searchInput');
+      el?.focus();
+    }, 1);
+
+    if(!show) {
+      this.searchValue = '';
+    }
+  }
+
+  onClick(e: Event) {
+    e.stopPropagation()
+  }
 
 }
