bd45562efc3403ee0055193499715f4e7e03da0e
Syafiq <isyafiq@gmail.com>
2024-03-30 22:05:10 +0800

    create pokemon search component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RandomPokemonComponent } from '../random-pokemon/random-pokemon.component';
+import { PokemonSearchComponent } from '../pokemon-search/pokemon-search.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, RandomPokemonComponent],
+  imports: [RouterModule, RandomPokemonComponent, PokemonSearchComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
---
+++ src/app/pokemon-search/pokemon-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-search.component.html',
+  styleUrl: './pokemon-search.component.css'
+})
+export class PokemonSearchComponent {
+
+}
