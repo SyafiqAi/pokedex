df5af189ed65661b45dd29de0c1c6ca69646492e
Syafiq <isyafiq@gmail.com>
2024-04-01 18:29:15 +0800

    sprites in search

--- src/app/pokemon-search/pokemon-search.component.ts
+++ src/app/pokemon-search/pokemon-search.component.ts
@@ -1,13 +1,15 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, SimpleChanges, computed, signal } from '@angular/core';
 import { PokedexService } from '../pokedex.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-search',
   standalone: true,
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './pokemon-search.component.html',
   styleUrl: './pokemon-search.component.css'
 })
@@ -21,15 +23,31 @@ export class PokemonSearchComponent {
   pokemonList: string[] = []
   searchValue = ''
   showModal = false;
+  filteredList: {name: string, url?: string}[] = [];
 
-
-  public get filteredList() {
+  public getFilteredList() {
     if(this.searchValue == '') {
-      return []
+      this.filteredList = [];
+      return;
     }
+    
     const pokemonPerSearch = 10
-    const filteredList = this.pokemonList;
-    return filteredList.filter(p => p.includes(this.searchValue.toLowerCase())).splice(0,pokemonPerSearch);
+    let list = this.pokemonList;
+    list = list.filter(p => p.includes(this.searchValue.toLowerCase())).splice(0,pokemonPerSearch)
+
+    this.filteredList = list.map(p => { return {name: p}});
+
+    this.filteredList.forEach(p => {
+      this.getPicUrl(p.name).subscribe({
+        next: url => {p.url = url}
+      })
+    })
+  }
+
+  getPicUrl(pokemon: string) {
+    return this.pokedexService.getPokemon(pokemon).pipe(
+      map(p => p.sprites.front_default)
+    )
   }
 
   toggleModal(show: boolean) {
@@ -44,6 +62,7 @@ export class PokemonSearchComponent {
 
     if(!show) {
       this.searchValue = '';
+      this.filteredList = [];
     }
   }
 
